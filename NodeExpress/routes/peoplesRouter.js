accb7654a648480555f7618cdc7a16cd1de178b7
sebastian prado <sebaslkjh@gmail.com>
2022-09-05 22:17:04 -0500

    creacion de endPoints: get, put, patch, delete para peoples y sus Services

--- NodeExpress/routes/peoplesRouter.js
+++ NodeExpress/routes/peoplesRouter.js
@@ -5,24 +5,58 @@ const router = express.Router();
 
 const people = new PeoplesService();
 
-router.get('/', (req, res) => {
-  const peoples = people.find();
-  res.status(200).json(peoples);
+router.get('/', async (req, res, next) => {
+  try {
+    const peoples = await people.find();
+    res.status(200).json(peoples);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.post('/', (req, res) => {
-  const { name, type } = req.body;
-  people.create(name, type);
-  res.status(201).send('people create correct');
+router.get(
+  '/:id',
+  // validatorHandler(getProductSchema, 'params'),
+  async (req, res, next) => {
+    const { id } = req.params; // parametros
+    try {
+      const peopleId = await people.findOne(id);
+      res.status(200).json(peopleId);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.post('/', async (req, res, next) => {
+  const body = req.body;
+  try {
+    const newPeople = await people.create(body);
+    res.status(201).send(newPeople);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.patch('/:id', async (req, res, next) => {
   const { id } = req.params;
-  res.json({
-    id,
-    name: 'felipe',
-    type: 'empleado',
-  });
+  const body = req.body;
+  try {
+    const peopleUpdate = await people.update(id, body);
+    res.status(201).json(peopleUpdate);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete('/:id', async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const deletePeople = await people.delete(id);
+    res.status(202).json(deletePeople);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
--- NodeExpress/services/peoplesServices.js
+++ NodeExpress/services/peoplesServices.js
@@ -1,3 +1,6 @@
+const faker = require('faker'); //traer libreria a archivo principal
+const boom = require('@hapi/boom');
+
 // aqui tendremos toda la logica de los routes
 class PeoplesService {
   constructor() {
@@ -5,32 +8,164 @@ class PeoplesService {
       {
         name: 'daniel',
         type: 'empleado',
+        age: 23,
+        area: 'almacen',
       },
       {
-        name: 'Sebastian',
+        name: 'Santiago',
         type: 'seguridad',
+        age: 25,
+        area: 'banco',
       },
       {
         name: 'felipe',
         type: 'empleado',
+        age: 32,
+        area: 'oficina',
+      },
+      {
+        name: 'daniela',
+        type: 'empleada',
+        age: 20,
+        area: 'oficina',
+      },
+      {
+        name: 'anguie',
+        type: 'empleado',
+        age: 22,
+        area: 'bodega',
+      },
+      {
+        name: 'jose',
+        type: 'empleado',
+        age: 52,
+        area: 'oficina',
+      },
+      {
+        name: 'fabian',
+        type: 'empleado',
+        age: 72,
+        area: 'bodega',
+      },
+      {
+        name: 'manuela',
+        type: 'empleado',
+        age: 42,
+        area: 'office',
+      },
+      {
+        name: 'maria',
+        type: 'empleado',
+        age: 28,
+        area: 'bodega',
+      },
+      {
+        name: 'paola',
+        type: 'empleado',
+        age: 41,
+        area: 'banco',
+      },
+      {
+        name: 'diego',
+        type: 'empleado',
+        age: 27,
+        area: 'banco',
+      },
+      {
+        name: 'ariel',
+        type: 'empleado',
+        age: 26,
+        area: 'bodega',
+      },
+      {
+        name: 'pedro',
+        type: 'empleado',
+        age: 35,
+        area: 'almacen',
+      },
+      {
+        name: 'lidia',
+        type: 'empleado',
+        age: 36,
+        area: 'office',
+      },
+      {
+        name: 'damian',
+        type: 'empleado',
+        age: 39,
+        area: 'banco',
+      },
+      {
+        name: 'sergio',
+        type: 'empleado',
+        age: 29,
+        area: 'office',
+      },
+      {
+        name: 'rosa',
+        type: 'empleado',
+        age: 26,
+        area: 'bodega',
       },
     ];
+    this.generateIds();
+  }
+  generateIds() {
+    for (let index = 0; index < this.peoples.length; index++) {
+      const people = this.peoples[index];
+      this.peoples[index] = {
+        id: faker.datatype.uuid(),
+        ...people,
+      };
+    }
   }
 
-  create(name, type) {
-    this.peoples.push({
-      name,
-      type,
+  create(data) {
+    let newPeople = {
+      id: faker.datatype.uuid(),
+      ...data,
+    };
+    this.peoples.push(newPeople);
+    return newPeople;
+  }
+
+  find() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(this.peoples);
+      }, 3000);
     });
   }
-  find() {
-    return this.peoples;
+
+  async findOne(id) {
+    const people = this.peoples.find((people) => people.id === id);
+    if (!people) {
+      throw boom.notFound('people not Found');
+    }
+    return people;
   }
-  findOne(id) {
-    return this.products.find((product) => product.id === id);
+
+  async update(id, changes) {
+    const index = this.peoples.findIndex((people) => people.id === id);
+    if (index === -1) {
+      throw boom.notFound('people not Found');
+    }
+    const people = this.peoples[index];
+    this.peoples[index] = {
+      ...people,
+      ...changes,
+    };
+    return this.peoples[index];
   }
-  update() {}
-  delete() {}
+  async delete(id) {
+    const index = this.peoples.findIndex((people) => people.id === id);
+    if (index === -1) {
+      //   throw new Error('Product not found');
+      throw boom.notFound('people not Found');
+    }
+    this.peoples.splice(index, 1);
+    return 'people delete';
+  }
 }
 
 module.exports = PeoplesService;
