a3967bac5099f233efeefe2d1549abfdbeff5b6a
sebastian prado <sebaslkjh@gmail.com>
2022-09-06 17:45:15 -0500

    cambios para Deploy

--- NodeExpress/index.js
+++ NodeExpress/index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 
 const routerApi = require('./routes/index');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000; // variable de entorno de heroku
 
 const {
   logErrors,
